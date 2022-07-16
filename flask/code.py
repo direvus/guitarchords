7ae9dfd46477fbeebd066d80dadbd7c648887847
Brendan Jurd <direvus@gmail.com>
2022-07-17 01:21:18 +1000

    Add Flask web application for generating chord diagrams.

---
+++ flask/code.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+import os
+import subprocess
+from copy import deepcopy
+from tempfile import NamedTemporaryFile
+from xml.etree import ElementTree
+
+import yaml
+from flask import Flask, request, render_template, send_file
+from generate_chord_diagrams import generate_chord
+
+
+prefix = '/guitarchords'
+app = Flask(__name__)
+urls = (
+        '/', 'index',
+        prefix, 'index',
+        prefix + '/', 'index',
+        )
+
+
+def strings_to_long_form(chord):
+    """Convert a chord's string definition into long form.
+
+    This means that every string will be a list of 2 elements, the fretting
+    followed by the finger, even for strings that are open or muted.
+    """
+    strings = []
+    for string in chord['strings']:
+        if isinstance(string, str):
+            strings.append(['', string])
+        else:
+            strings.append(string)
+    result = deepcopy(chord)
+    result['strings'] = strings
+    return result
+
+
+def strings_to_short_form(chord):
+    """Convert a chord's string definition into short form.
+
+    This means that only fretted strings will be a list of 2 elements, open or
+    muted strings will be a single-character string 'O' or 'X'.
+    """
+    strings = []
+    for finger, fret in chord['strings']:
+        if fret in {'X', 'O'}:
+            strings.append(fret)
+        else:
+            strings.append([finger, fret])
+    result = deepcopy(chord)
+    result['strings'] = strings
+    return result
+
+
+def validate_chord(request):
+    name = request.args.get('name', '').strip()
+    strings = []
+    for n in range(1, 7):
+        fret = request.args.get(f's{n}', 'O').strip().upper()
+        finger = request.args.get(f'f{n}', '').strip().upper()
+        if fret in {'M', 'X'}:
+            fret = 'X'
+        elif finger and fret.isdigit():
+            fret = int(fret)
+            if finger.isdigit():
+                finger = int(finger)
+        strings.append([finger, fret])
+    return {
+            'name': name,
+            'strings': strings,
+            }
+
+
+def generate_diagram(chord):
+    tree = ElementTree.parse('chord_base.svg')
+    diag = generate_chord(tree, chord)
+    svgfile = NamedTemporaryFile(delete=False)
+    diag.write(svgfile)
+    svgfile.close()
+    pngfile = NamedTemporaryFile(delete=False)
+    pngfile.close()
+    subprocess.run([
+            'inkscape',
+            '-z',
+            '-C',
+            '-w',
+            '209',
+            '-e',
+            pngfile.name,
+            svgfile.name,
+            ])
+    os.remove(svgfile.name)
+    return pngfile.name
+
+
+@app.route('/')
+def index():
+    with open('chords.yaml', 'r') as fp:
+        presets = yaml.safe_load(fp)
+    initials = []
+    groups = {}
+    for chord in presets:
+        chord = strings_to_long_form(chord)
+        initial = chord['name'][0].upper()
+        if initial in groups:
+            groups[initial].append(chord)
+        else:
+            groups[initial] = [chord]
+            initials.append(initial)
+
+    presets = [(x, groups[x]) for x in initials]
+    chord = validate_chord(request)
+    return render_template(
+            'index.html',
+            name=chord['name'],
+            strings=chord['strings'],
+            presets=presets,
+            )
+
+
+@app.route('/chord')
+def chord():
+    chord = validate_chord(request)
+    chord = strings_to_short_form(chord)
+    print(chord)
+    filename = generate_diagram(chord)
+    result = send_file(filename, 'image/png')
+    os.remove(filename)
+    return result
---
+++ flask/generate_chord_diagrams.py
@@ -0,0 +1,1 @@
+../generate_chord_diagrams.py
